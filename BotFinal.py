b5355c9b4600a8da28b44011d4edcec44a5da617
IldarGisGaliev <ildargisgaliev@gmail.com>
2023-11-25 21:45:01 +0600

    first attaches

--- BotFinal.py
+++ BotFinal.py
@@ -11,49 +11,58 @@ from telegram.ext import (
     filters,
 )
 
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
 # Определим состояния разговора
 EMAIL, MESSAGE, FILE = range(3)
 
 # Функция для обработки команды /start
-def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    update.message.reply_text('Привет! Пожалуйста, отправь мне свой email.')
-    return EMAIL
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+   await update.message.reply_text('Привет! Пожалуйста, отправь мне свой email.')
+   return EMAIL
 
 # Функция для обработки полученного email
-def get_email(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def get_email(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     email = update.message.text
     context.user_data['email'] = email
-    update.message.reply_text(f'Спасибо! Теперь напиши мне своё сообщение.')
+    await update.message.reply_text(f'Спасибо! Теперь напиши мне своё сообщение.')
     return MESSAGE
 
 # Функция для обработки сообщения
-def get_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def get_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     message = update.message.text
     context.user_data['message'] = message
-    update.message.reply_text(f'Теперь прикрепи файл (картинку или PDF). Он должен быть меньше 2 МБ.')
+    await update.message.reply_text(f'Теперь прикрепи файл (картинку или PDF). Он должен быть меньше 2 МБ.')
     return FILE
 
 # Функция для обработки прикрепленного файла
-def get_file(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def get_file(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     file = update.message.document or update.message.photo[-1]
     file_size = file.file_size
 
     if file_size > 2 * 1024 * 1024:
-        update.message.reply_text('Файл слишком большой. Пожалуйста, отправь файл размером меньше 2 МБ.')
+        await update.message.reply_text('Файл слишком большой. Пожалуйста, отправь файл размером меньше 2 МБ.')
         return FILE
 
     file_extension = file.file_name.split('.')[-1].lower()
     if file_extension not in ['jpg', 'jpeg', 'png', 'pdf']:
-        update.message.reply_text('Неверный формат файла. Пожалуйста, отправь картинку или PDF.')
+        await update.message.reply_text('Неверный формат файла. Пожалуйста, отправь картинку или PDF.')
         return FILE
 
     context.user_data['file'] = file
-    update.message.reply_text('Файл получен!')
+    await update.message.reply_text('Файл получен!')
     return ConversationHandler.END
 
 # Функция для выхода из разговора
-def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    update.message.reply_text('До свидания!')
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    await update.message.reply_text('До свидания!')
     return ConversationHandler.END
 
 def main():
